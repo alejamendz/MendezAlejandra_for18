2286d91bf253470d3b34730db2a0c80f9e962e55
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-02-24 19:13:42 -0600

    Uso de for y fin del programa

--- MendezAlejandra_for18/Program.cs
+++ MendezAlejandra_for18/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pedir 5 números y mostrar el número mayor ingresado);
+Console.WriteLine("Pedir 5 números y mostrar el número mayor ingresado");
+
+
+int mayor = int.MinValue;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("Ingrese número: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > mayor)
+        mayor = num;
+}
+
+Console.WriteLine("Mayor: " + mayor);
